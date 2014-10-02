54c548dfe6a448ea7af92c34a800eb641fd5e94f
Richard Doktorics <doktoricsrichard@gmail.com>
2014-10-02 10:17:16 +0200

    init

---
+++ src/main/java/com/sequenceiq/gcc/GccTesterMain.java
@@ -0,0 +1,98 @@
+package com.sequenceiq.gcc;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.StringReader;
+import java.security.GeneralSecurityException;
+import java.security.KeyPair;
+import java.security.PublicKey;
+import java.security.Security;
+import java.util.Arrays;
+import java.util.List;
+
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.openssl.PEMReader;
+
+import com.github.fommil.ssh.SshRsaCrypto;
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.services.compute.Compute;
+import com.google.api.services.compute.ComputeScopes;
+import com.google.api.services.compute.model.Image;
+import com.google.api.services.compute.model.ImageList;
+import com.google.api.services.storage.StorageScopes;
+
+public class GccTesterMain {
+
+    private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+    private static final List<String> SCOPES = Arrays.asList(ComputeScopes.COMPUTE, StorageScopes.DEVSTORAGE_FULL_CONTROL);
+
+    private static String key = "";
+   private static String serviceAccountId = "";
+    private static String projectId = "";
+
+    public static void main(String[] args) {
+        validate(key);
+        Compute compute = buildCompute(projectId, key, serviceAccountId);
+
+        if(compute == null){
+            System.out.println("Your credentials are invalid...");
+            System.exit(1);
+        } else {
+            try {
+                Compute.Images.List list = compute.images().list(projectId);
+                ImageList execute = list.execute();
+                for(Image image : execute.getItems()) {
+                    System.out.println(image.getName());
+                }
+            } catch (IOException e) {
+                System.out.println("Your credentials are invalid: " + e.getMessage());
+                System.out.println(e);
+                System.exit(1);
+            }
+        }
+        System.out.println("Your credential is ok...");
+    }
+
+    public static Compute buildCompute(String projectid, String key, String serviceAccountId) {
+        try {
+            HttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
+            BufferedReader br = new BufferedReader(new StringReader(key));
+            Security.addProvider(new BouncyCastleProvider());
+            KeyPair kp = (KeyPair) new PEMReader(br).readObject();
+            GoogleCredential credential = new GoogleCredential.Builder().setTransport(httpTransport)
+                    .setJsonFactory(JSON_FACTORY)
+                    .setServiceAccountId(serviceAccountId)
+                    .setServiceAccountScopes(SCOPES)
+                    .setServiceAccountPrivateKey(kp.getPrivate())
+                    .build();
+            Compute compute = new Compute.Builder(
+                    httpTransport, JSON_FACTORY, null).setApplicationName("myapp")
+                    .setHttpRequestInitializer(credential)
+                    .build();
+            return compute;
+        } catch (GeneralSecurityException e) {
+            System.out.println("General security exception: " + e.getMessage());
+            System.exit(1);
+        } catch (IOException e) {
+            System.out.println("Ioexception under the compute creation: " + e.getMessage());
+            System.exit(1);
+        } catch (Exception e) {
+            System.out.println("Error occured: " + e.getMessage());
+            System.exit(1);
+        }
+        return null;
+    }
+
+    public static void validate(String key) {
+        try {
+            SshRsaCrypto rsa = new SshRsaCrypto();
+            PublicKey publicKey = rsa.readPublicKey(rsa.slurpPublicKey(key));
+        } catch (Exception e) {
+            String errorMessage = String.format("Could not validate publickey certificate  %s", key);
+        }
+    }
+}
